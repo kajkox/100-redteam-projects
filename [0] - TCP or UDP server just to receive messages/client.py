3a26018b4799898a84e0b1ad869653cf66ac2c75
kajkox <kajtek2004@gmail.com>
2024-06-26 18:13:38 +0200

    added .gitignore file

--- [0] - TCP or UDP server just to receive messages/client.py
+++ [0] - TCP or UDP server just to receive messages/client.py
@@ -1,14 +1,11 @@
 import socket
 
-def connectToServer():
-    HOST_ADD = "127.0.0.1"
-    HOST_PORT = 65432
+HOST_ADD = "127.0.0.1"
+HOST_PORT = 65432
 
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((HOST_ADD, HOST_PORT))
-        s.sendall("Hello, world")
-        data = s.recv(1024)
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST_ADD, HOST_PORT))
+    s.sendall("Hello, world")
+    data = s.recv(1024)
 
     print(f"Received {data}!")
-
-connectToServer()
--- [0] - TCP or UDP server just to receive messages/server.py
+++ [0] - TCP or UDP server just to receive messages/server.py
@@ -1,21 +1,15 @@
 import socket
 
-def setupServer():
-    try:
-        HOST_ADD = "127.0.0.1"
-        HOST_PORT = 65432
+HOST_ADD = "127.0.0.1"
+HOST_PORT = 65432
 
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.bind((HOST_ADD, HOST_PORT))
-            s.listen()
-            connection, address = s.accept()
-            with connection:
-                while True:
-                    data = connection.recv(1024)
-                    if not data:
-                        break
-                    connection.sendall(data)
-    except KeyboardInterrupt:
-        return 0
-    
-setupServer()
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST_ADD, HOST_PORT))
+    s.listen()
+    connection, address = s.accept()
+    with connection:
+        while True:
+            data = connection.recv(1024)
+            if not data:
+                break
+            connection.sendall(data)
