2daa61b5a7533fecab60af56652975ca0ab9dc75
kajkox <kajtek2004@gmail.com>
2024-06-26 19:02:52 +0200

    added final comments

--- [0] - TCP or UDP server just to receive messages/client.py
+++ [0] - TCP or UDP server just to receive messages/client.py
@@ -1,10 +1,10 @@
 import socket
 
-HOST_ADD = "127.0.0.1"
-HOST_PORT = 65432
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST_ADD, HOST_PORT))
-    s.sendall(b"Hello, world")
-    data = s.recv(1024)
+HOST_ADD = "127.0.0.1"  # 
+HOST_PORT = 65432   # constants where to connect
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:    # creating a socket object compliant with the context manager
+    s.connect((HOST_ADD, HOST_PORT))    # connecting to the socket, with the addres, port tuple
+    s.sendall(b"Hello, world")  # sending the message in byte format
+    data = s.recv(1024) # receiving the data from the client with 1024 buffer size
 
-    print(f"Received {data}!")
+    print(f"Received {data}!")  # printing the message
--- [0] - TCP or UDP server just to receive messages/server.py
+++ [0] - TCP or UDP server just to receive messages/server.py
@@ -3,14 +3,14 @@ import socket
 HOST_ADD = "127.0.0.1"  #
 HOST_PORT = 65432   # constants on where to bind the socket
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:    # 
-    s.bind((HOST_ADD, HOST_PORT))
-    s.listen()
-    connection, address = s.accept()
-    with connection:
-        print(f"Connected by {address}!")
-        while True:
-            data = connection.recv(1024)
-            if not data:
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:    # socket.socket returns a socket object that supports context manager, so using with keyword
+    s.bind((HOST_ADD, HOST_PORT))   # binding the socket object to addres, port tuple
+    s.listen()  # listening for connections
+    connection, address = s.accept()    # accept method of the socket object returns two objects, one for the connection, one for the address
+    with connection:    # also supports context manager, so using with
+        print(f"Connected by {address}!")   # printing the client address
+        while True: # while loop, here reduntant because the client disconnects after receiving the message
+            data = connection.recv(1024)    # receiving the message from the client with 1024 buffer size
+            if not data:    # break case, if the data sent is empty => "b''"
                 break
-            connection.sendall(data)
+            connection.sendall(data)    # sending back the message from the client
