4859349af6f2f716870cbe68f97af92d3aa5d7bd
kajkox <kajtek2004@gmail.com>
2024-06-26 18:22:57 +0200

    Simple echo back and forth

--- [0] - TCP or UDP server just to receive messages/client.py
+++ [0] - TCP or UDP server just to receive messages/client.py
@@ -5,7 +5,7 @@ HOST_PORT = 65432
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST_ADD, HOST_PORT))
-    s.sendall("Hello, world")
+    s.sendall(b"Hello, world")
     data = s.recv(1024)
 
     print(f"Received {data}!")
--- [0] - TCP or UDP server just to receive messages/server.py
+++ [0] - TCP or UDP server just to receive messages/server.py
@@ -8,6 +8,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.listen()
     connection, address = s.accept()
     with connection:
+        print(f"Connected by {address}!")
         while True:
             data = connection.recv(1024)
             if not data:
