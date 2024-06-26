38b5ba6c426732bcddfa5af72a47a243e5a9826a
kajkox <kajtek2004@gmail.com>
2024-06-26 18:51:00 +0200

    final version, adding commenta

--- [0] - TCP or UDP server just to receive messages/client.py
+++ [0] - TCP or UDP server just to receive messages/client.py
@@ -2,7 +2,6 @@ import socket
 
 HOST_ADD = "127.0.0.1"
 HOST_PORT = 65432
-
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST_ADD, HOST_PORT))
     s.sendall(b"Hello, world")
--- [0] - TCP or UDP server just to receive messages/server.py
+++ [0] - TCP or UDP server just to receive messages/server.py
@@ -1,9 +1,9 @@
 import socket
 
-HOST_ADD = "127.0.0.1"
-HOST_PORT = 65432
+HOST_ADD = "127.0.0.1"  #
+HOST_PORT = 65432   # constants on where to bind the socket
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:    # 
     s.bind((HOST_ADD, HOST_PORT))
     s.listen()
     connection, address = s.accept()
