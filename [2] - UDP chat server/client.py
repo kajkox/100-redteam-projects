87b51737950fea97e2b9b5f5edc84fa5e560c6e2
kajkox <kajtek2004@gmail.com>
2024-07-06 18:00:23 +0200

    idk

--- [2] - UDP chat server/client.py
+++ [2] - UDP chat server/client.py
@@ -1,8 +1,19 @@
 import socket
 
-TRANSPORT_PROT = socket.SOCK_DGRAM
-ADDRESS_FAM = socket.AF_INET
-IP_ADD = '127.0.0.1'
-PORT = 21372
+CLIENT_ADD = '127.0.0.1'
+CLIENT_PORT = 21372
+SERVER_ADD = '127.0.0.1'
+SERVER_PORT = 21371
 
-socketObject = socket.socket(family=ADDRESS_FAM, type=TRANSPORT_PROT)
+TRANPORT_PROT = socket.SOCK_DGRAM
+IP_FAMILY = socket.AF_INET
+
+clientSocket = socket.socket(family=IP_FAMILY, type=TRANPORT_PROT)
+clientSocket.bind((CLIENT_ADD, CLIENT_PORT))
+
+while True:
+    send_data = input(">")
+    send_data = send_data.encode()
+    clientSocket.sendto(send_data, (SERVER_ADD, SERVER_PORT))
+    data, address = clientSocket.recvfrom(1024)
+    print(data.decode())
--- [2] - UDP chat server/server.py
+++ [2] - UDP chat server/server.py
@@ -1,8 +1,19 @@
 import socket
 
-TRANSPORT_PROT = socket.SOCK_DGRAM
-ADDRESS_FAM = socket.AF_INET
-IP_ADD = '127.0.0.1'
-PORT = 21372
+SERVER_ADD = '127.0.0.1'
+SERVER_PORT = 21371
+CLIENT_ADD = '127.0.0.1'
+CLIENT_PORT = 21372
 
-socketObject = socket.socket(family=ADDRESS_FAM, type=TRANSPORT_PROT)
+TRANSPORT_PROT = socket.SOCK_DGRAM
+IP_FAM = socket.AF_INET
+
+serverSocket = socket.socket(family=IP_FAM, type=TRANSPORT_PROT)
+serverSocket.bind((SERVER_ADD, SERVER_PORT))
+
+while True:
+    recv_data, address = serverSocket.recvfrom(1024)
+    print(recv_data.decode())
+    send_data = input(">")
+    send_data = send_data.encode()
+    serverSocket.sendto(send_data, (CLIENT_ADD, CLIENT_PORT))
