4805428b480a50485de12ccec28e615dc5e5ac14
kajkox <kajtek2004@gmail.com>
2024-07-06 18:44:54 +0200

    better syntax

--- [2] - UDP chat server/client.py
+++ [2] - UDP chat server/client.py
@@ -5,15 +5,24 @@ CLIENT_PORT = 21372
 SERVER_ADD = '127.0.0.1'
 SERVER_PORT = 21371
 
-TRANPORT_PROT = socket.SOCK_DGRAM
-IP_FAMILY = socket.AF_INET
+TRANPORT_PROT = socket.SOCK_DGRAM # UDP
+IP_FAMILY = socket.AF_INET # IPv4
 
 clientSocket = socket.socket(family=IP_FAMILY, type=TRANPORT_PROT)
 clientSocket.bind((CLIENT_ADD, CLIENT_PORT))
+print(f"[CLIENT] Bound succesfully to {(CLIENT_ADD, CLIENT_PORT)}")
 
 while True:
     send_data = input(">")
+    if send_data == "close":
+        print(f"Connection with {(SERVER_ADD, SERVER_PORT)} closed by localhost!")
+        break
+
     send_data = send_data.encode()
     clientSocket.sendto(send_data, (SERVER_ADD, SERVER_PORT))
-    data, address = clientSocket.recvfrom(1024)
-    print(data.decode())
+    recv_data, address = clientSocket.recvfrom(1024)
+    if recv_data == "close":
+        print(f"Connection with {address} closed by server!")
+        break
+
+    print(recv_data.decode())
