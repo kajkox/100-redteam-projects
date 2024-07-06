a231ac4a60dd5153e7cb0d20d4035dd094ed19d6
kajkox <kajtek2004@gmail.com>
2024-07-06 18:02:17 +0200

    final version

--- [2] - UDP chat server/server.py
+++ [2] - UDP chat server/server.py
@@ -5,8 +5,8 @@ SERVER_PORT = 21371
 CLIENT_ADD = '127.0.0.1'
 CLIENT_PORT = 21372
 
-TRANSPORT_PROT = socket.SOCK_DGRAM
-IP_FAM = socket.AF_INET
+TRANSPORT_PROT = socket.SOCK_DGRAM # UDP
+IP_FAM = socket.AF_INET # IPv4
 
 serverSocket = socket.socket(family=IP_FAM, type=TRANSPORT_PROT)
 serverSocket.bind((SERVER_ADD, SERVER_PORT))
