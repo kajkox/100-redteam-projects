796b9898037b7d59fdaf5e97277a767315aa1a5e
kajkox <kajtek2004@gmail.com>
2024-10-02 22:25:35 +0200

    created [3], updated README

--- [3] - Multi-threaded UDP or TCP chat server/tcp_server.py
+++ [3] - Multi-threaded UDP or TCP chat server/tcp_server.py
@@ -5,7 +5,8 @@ import socket
 # will it work on tcp? sure why not
 # the first client will establish a connection with the server, the server stores the data temporarily, and "switches" to being a client
 # now, the receivinig client will need to be already set up, maybe have a function that implements that? could be also called
-# so, the server will 
+# so, the server will then send the data to the end, although it would be easier to do on udp, but i think i will be able to handle it
+# we will see
 # 
 
 # constants
