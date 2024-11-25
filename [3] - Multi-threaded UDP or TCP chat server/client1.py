dd0be247f171bd0a595e14ade5e68364e34f6930
kajkox <kajtek2004@gmail.com>
2024-11-25 01:06:18 +0100

    added friend adding, init config file

--- [3] - Multi-threaded UDP or TCP chat server/client1.py
+++ [3] - Multi-threaded UDP or TCP chat server/client1.py
@@ -1,5 +1,26 @@
 import socket
 
+## CONSTANTS
+
+# client constants
+CLIENT_IP = '127.0.0.1'
+CLIENT_PORT = 34568
+
+# these constants make creating socket objects easier
+from config import SERVER_PORT, SERVER_IP, ADD_FAM, S_TYPE
+
+# these define the types of messages sent
+from config import FR_ADD_IP, FR_ADD_NM
+
+# error messages
+from config import ERR_FR_ADD_IP, ERR_FR_ADD_NM
+
+# TODO 
+# add friend by ip - DONE
+# add friend by name - DONE
+# displaying contact book
+# actually sending messages
+
 # main menu prompt
 MAIN_MENU: str = """
 ---Welcome to my UDP chat server---
@@ -10,45 +31,184 @@ Please select an option from the menu below:
 Your choice: """
 
 
+## GLOBAL VARIABLES
+
+# contant book (might implement it in a file)
+contact_book = {
+
+}
+
 # function that adds friend by name to the contact book
-def name_add(friend_name: str) -> None | SyntaxError:
-    print(f"name works: {friend_name}")
+# the function sends data to the server with some metadata to interpret what type of request it is
+# the same method is used in ip_add()
+def name_add(friend_name: str) -> None | SyntaxError | NameError | ValueError:
+
+    # checking if name is already in contact book
+    if friend_name in contact_book.keys():
+        raise NameError
+
+
+    # creating a socket object and binding
+    s_obj = socket.socket(family=ADD_FAM, type=S_TYPE)
+    s_obj.bind((CLIENT_IP, CLIENT_PORT))
+    
+    # crafting the message
+    message = ""
+    message += friend_name
+    message += FR_ADD_NM
+
+    # encoding to bytes
+    message = message.encode()
+
+
+    # sending the message and waiting for response
+    s_obj.sendto(message, (SERVER_IP, SERVER_PORT))
+    response = s_obj.recv(1024)
+    response = response.decode()
+
+
+    # parsing data, comparing response
+    parsed_response: list[str] = response.split("[TYPE]:")
+    match parsed_response[1]:
+
+        # the server responded with an error
+        case "EFNM":
+            raise SyntaxError
+        
+        # the server responded properly with a (ip, port) response tuple
+        case "OK":
+            # extracting the ip and port from message
+            friend_ip = parsed_response[0].split(',')[0]
+            friend_port = int(parsed_response[0].split(',')[1])
+
+            # adding to contact book
+            contact_book[friend_name] = (friend_ip, friend_port)
+            return None
+        
+        # unexpected or no response
+        case _:
+            raise ValueError
+
 
 
 # funtction that adds friend by ip, port pair to contact book
-def ip_add(friend_ip: str, friend_port: int) -> None | SyntaxError:
-    print(f"IP works: {friend_ip}, {friend_port}")
+def ip_add(friend_ip: str, friend_port: int) -> None | SyntaxError | NameError | ValueError:
+
+    # checking if name is already in contact book
+    if (friend_ip, friend_port) in contact_book.values():
+        raise NameError
+    
+    # creating a socket object and binding
+    s_obj = socket.socket(family=ADD_FAM, type=S_TYPE)
+    s_obj.bind((CLIENT_IP, CLIENT_PORT))
+
+    # crafting the message
+    message: str = ""
+    message += friend_ip + ',' + friend_port
+    message += FR_ADD_IP
+
+    # encoding to bytes
+    message = message.encode()
+
+
+    # sending the message and waiting for response
+    s_obj.sendto(message, (SERVER_IP, SERVER_PORT))
+    response = s_obj.recv(1024)
+    response = response.decode()
+
+
+    # parsing data, comparing response
+    parsed_response: list[str] = response.splt("[TYPE]:")
+    match parsed_response[1]:
+
+        # the server responded with an error
+        case "EFIP":
+            raise SyntaxError
+        
+        # the server responded properly with a name of the friend
+        case "OK":
+            friend_name = parsed_response[0]
+
+            # adding to contanct book
+            contact_book[friend_name] = (friend_ip, friend_port)
+            return None
+        
+        # unexpected or no response
+        case _:
+            raise ValueError
+
+    # print(f"IP works: {friend_ip}, {friend_port}")
 
 # main menu loop
 def main() -> int:
+
     while True:
         print(f"{MAIN_MENU}")
+
+        # catching wrong input
         try:
             choice = int(input())
         except ValueError:
             print("Please input a number.")
+
+        # menu selection
         match choice:
+
             case 0:
                 return 0
+
             case 1:
                 print(f"Please input the name of the friend you want to add: ")
                 name = input()
+
+                # catching error from function
                 try:
+
+                    # adding friend
                     name_add(name)
+                
+                # server couldn't add friend
                 except SyntaxError:
                     print("[ERROR] Friend not found, make sure you typed the name correctly.")
+
+                # friend already in contact book
+                except NameError:
+                    print(f"[ERROR] Friend is already in your contact book.")
+
+                # unexpected or no response from server
+                except ValueError:
+                    print(f"[ERROR] The server didn't respond properly, make sure your connection is stable.")
+
+
             case 2:
                 print(f"Please input your friend's ip: ")
                 ip = input()
                 print(f"Please input the port: ")
+
+                # catching wrong input
                 try:
                     port = int(input)
                 except ValueError:
                     print("[ERROR] Please input the port as a number.")
+
+                # catching error from function
                 try:
+
+                    # adding friend
                     ip_add(ip, port)
+
+                # server couldn't add friend
                 except SyntaxError:
                     print(f"[ERROR] Friend not found, make sure you typed the ip and port correctly.")
+
+                # friend already in contact book
+                except NameError:
+                    print(f"[ERROR]: Friend is already in your contact book.")
+
+                # unexpected or no response from server
+                except ValueError:
+                    print(f"[ERROR] The server didn't respond properly, make sure your connection is stable")
+
             case _:
                 print(f"Option not found, make sure you typed the option correctly")
 
---
+++ [3] - Multi-threaded UDP or TCP chat server/config.py
@@ -0,0 +1,23 @@
+### this file contains constants that are used throughout the project
+import socket
+
+# socket constants
+SERVER_IP = '127.0.0.1'
+SERVER_PORT = 34567
+ADD_FAM = socket.AF_INET
+S_TYPE = socket.SOCK_DGRAM
+
+# message constants
+FR_ADD_IP = "[TYPE]:FIP"
+FR_ADD_NM = "[TYPE]:FNM"
+OK = "[TYPE]:OK"
+# error constants
+ERR_FR_ADD_IP = "[TYPE]:EFIP"
+ERR_FR_ADD_NM = "[TYPE]:EFNM"
+
+## the format below specifies the type and message that is being sent by the client/user
+# the request type is specified in the end, and is seperated by a special character combination to ensure that the user doesn't accidentally mess up
+# the syntax is
+    # <message/data>[TYPE]:<type>
+    # the server then parses this data and uses the appropriate function depending on the <type>
+    # also the client parses the answer and know when to raise a error or include the data
