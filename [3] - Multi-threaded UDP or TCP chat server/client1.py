bc3dce96baddcdfad7587c43286e90bd0a8c2ab7
Kajetan Dzik <kajtek2004@gmail.com>
2025-10-22 17:42:50 +0200

    adding macros file

--- [3] - Multi-threaded UDP or TCP chat server/client1.py
+++ [3] - Multi-threaded UDP or TCP chat server/client1.py
@@ -1,8 +1,18 @@
 import socket
+from macros.py import *
 
 # macros
 SERVER_IP = "127.0.0.1"
 SERVER_PORT = 54321
 
+CLIENT_IP = "127.0.0.1"
+CLIENT_PORT =
 
-socket_obj = socket.socket
+IPv4 = socket.AF_INET
+TCP = socket.SOCK_STREAM
+
+# creating
+socket_obj = socket.socket(IPv4, TCP)
+
+# binding
+socket_obj.bind()
